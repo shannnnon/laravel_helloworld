30debb67733cd6fb8f375bbc18688333041976ce
vagrant <vagrant@homestead>
2018-11-05 08:08:15 +0000

    finish static pages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/**
+ * GET 常用于页面读取
+ * POST 常用于数据提交
+ * PATCH 常用于数据更新
+ * DELETE 常用于数据删除
+ *
+ *
+ */
+
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
+
